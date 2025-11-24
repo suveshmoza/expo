bb399d9e24bd14a0e2f1e1b20dd40fa8b39fa815
Suvesh Moza <hellosuvesh@gmail.com>
2025-11-25 02:06:32 +0530

    Apply suggestions from code review

--- packages/expo-ui/android/src/main/java/expo/modules/ui/ExpoUIModule.kt
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/ExpoUIModule.kt
@@ -28,7 +28,6 @@ import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.viewevent.getValue
 import expo.modules.ui.button.Button
 import expo.modules.ui.menu.ContextMenu
-import kotlin.reflect.KProperty
 
 class ExpoUIModule : Module() {
   override fun definition() = ModuleDefinition {
